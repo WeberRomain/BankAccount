eb5de998d9e2e95039d5ad1109fc1896e11f1674
Romain W <romain.weber@carbon-it.com>
2022-11-10 15:36:44 +0100

    factorization of withdraw and deposit method

--- src/main/java/io/weber/service/NoOverdraftAccountService.java
+++ src/main/java/io/weber/service/NoOverdraftAccountService.java
@@ -23,36 +23,41 @@ public class NoOverdraftAccountService implements BankService{
 
     @Override
     public Transaction deposit(UUID id, BigDecimal amount){
-        if(!accountRepository.ifAccountExist(id)){
-            throw new NotFoundAccountException("Account id not found");
-        }
-        if(amount.compareTo(BigDecimal.ZERO) <=0){
-            throw new InvalidTransactionException("Amount can't be negative or zero");
-        }
+        validateInput(id,amount);
         var amountScale = amount.setScale(2, RoundingMode.HALF_EVEN);
-        var lastTransaction = transactionRepository.getLastTransaction(id);
-        var balance = lastTransaction.map(Transaction::accountBalance).orElse(BigDecimal.ZERO)
-                .setScale(2, RoundingMode.HALF_EVEN);
-        return transactionRepository.addTransaction(new Transaction(id,LocalDate.now(),amountScale,TransactionType.DEPOSIT,balance.add(amountScale)));
+        var balance = getLastBalance(id);
+        return addTransaction(id,LocalDate.now(),amountScale,TransactionType.DEPOSIT,balance.add(amountScale));
     }
 
     @Override
     public Transaction withdraw(UUID id, BigDecimal amount) {
-        if(!accountRepository.ifAccountExist(id)){
-            throw new NotFoundAccountException("Account id not found");
-        }
-        if(amount.compareTo(BigDecimal.ZERO) <=0){
-            throw new InvalidTransactionException("Amount can't be negative or zero");
-        }
+        validateInput(id,amount);
         var amountScale = amount.setScale(2, RoundingMode.HALF_EVEN);
-        var lastTransaction = transactionRepository.getLastTransaction(id);
-        var balance = lastTransaction.map(Transaction::accountBalance).orElse(BigDecimal.ZERO)
-                .setScale(2, RoundingMode.HALF_EVEN);
+        var balance = getLastBalance(id);
         if(balance.equals(BigDecimal.ZERO)){
             throw new OverdraftException("Balance is empty");
         } else if (balance.subtract(amountScale).compareTo(BigDecimal.ZERO)<0) {
             throw new InvalidTransactionException("Balance can't be negative");
         }
-        return transactionRepository.addTransaction(new Transaction(id,LocalDate.now(),amountScale,TransactionType.WITHDRAW,balance.subtract(amountScale)));
+        return addTransaction(id,LocalDate.now(),amountScale,TransactionType.WITHDRAW,balance.subtract(amountScale));
+    }
+
+    private void validateInput(UUID id, BigDecimal amount){
+        if(!accountRepository.ifAccountExist(id)){
+            throw new NotFoundAccountException("Account id not found");
+        }
+        if(amount.compareTo(BigDecimal.ZERO) <=0){
+            throw new InvalidTransactionException("Amount can't be negative or zero");
+        }
+    }
+
+    private BigDecimal getLastBalance(UUID id){
+        var lastTransaction = transactionRepository.getLastTransaction(id);
+        return lastTransaction.map(Transaction::accountBalance).orElse(BigDecimal.ZERO)
+                .setScale(2, RoundingMode.HALF_EVEN);
+    }
+
+    private Transaction addTransaction(UUID id, LocalDate date, BigDecimal amount, TransactionType type, BigDecimal balance){
+        return transactionRepository.addTransaction(new Transaction(id,date,amount,type,balance));
     }
 }
